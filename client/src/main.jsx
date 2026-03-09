74be09a3aad032ff7c92dd430698b1da6d114511
llparagranell <paragrane000@gmail.com>
2026-03-09 18:12:09 +0530

    frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom";
 import "./index.css"
 import App from "./App.jsx"
 import { ThemeProvider } from "./context/ThemeContext.jsx"
@@ -8,12 +8,12 @@ import { AuthProvider } from "./context/AuthContext.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/saas/",
+
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true
+        target: "https://saas-wtcn.onrender.com",
+        changeOrigin: true,
+        secure: true
       }
     }
   }
-})
+});
